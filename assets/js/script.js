27e2ee0a36a8ad669b4ca0ee127789a1377c537b
raffaele-catalano <raf.catalano@gmail.com>
2023-04-14 17:43:34 +0200

    added v-for for tasks and deleteTasks()

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,28 @@ const {createApp} = Vue;
 createApp ({
     data(){
         return{
-
+            tasks: [
+                {
+                    text: 'Ricordare ad Andrea di dar da mangiare ai cavalli',
+                    done: false
+                },
+                {
+                    text: 'Comprare un profumo al Pilla Store',
+                    done: true
+                },
+                {
+                    text: 'Dire al Teacher che sulla Marinara si mette origano e non rosmarino',
+                    done: false
+                },
+            ],
+            shake: false,
         }
     },
 
     methods: {
-
+        deleteTask (index){
+            this.tasks.splice(index, 1)
+        },
     },
 
     mounted (){
